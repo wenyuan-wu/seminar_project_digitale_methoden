e69230049904ef7fcdefebe39444cafc2eb5d383
Wenyuan Wu <wenyuan.wu@uzh.ch>
2021-05-19 08:13:55 +0200

    training data set

--- corpus_inspection.py
+++ corpus_inspection.py
@@ -4,3 +4,58 @@ import matplotlib.pyplot as plt
 col_names = ["hist", "norm", "year"]
 df = pd.read_csv("data/breton_devri.csv", sep=",", names=col_names)
 
+print(len(df))
+
+x_labels = ["avant 1100",
+            "1100 - 1449",
+            "1450 - 1559",
+            "1600 - 1650",
+            "1651 - 1799",
+            "1800 - 1899",
+            "1900 - 1999",
+            "depuis 2000"
+            ]
+
+frequencies = []
+
+df_1 = df[df["year"] < 1100]
+frequencies.append(len(df_1))
+
+df_2 = df[df["year"].between(1100, 1450)]
+frequencies.append(len(df_2))
+
+df_3 = df[df["year"].between(1450, 1600)]
+frequencies.append(len(df_3))
+
+df_4 = df[df["year"].between(1600, 1651)]
+frequencies.append(len(df_4))
+
+df_5 = df[df["year"].between(1651, 1800)]
+frequencies.append(len(df_5))
+
+df_6 = df[df["year"].between(1800, 1900)]
+frequencies.append(len(df_6))
+
+df_7 = df[df["year"].between(1900, 2000)]
+frequencies.append(len(df_7))
+
+df_8 = df[df["year"] > 2000]
+frequencies.append(len(df_8))
+
+freq_series = pd.Series(frequencies)
+
+# Plot the figure.
+plt.figure(figsize=(12, 8))
+ax = freq_series.plot(kind='bar')
+ax.set_title('Period Distribution')
+ax.set_ylabel('Amount')
+ax.set_xticklabels(x_labels, rotation="horizontal")
+
+rects = ax.patches
+
+for rect, val in zip(rects, frequencies):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height + 5, val,
+            ha='center', va='bottom')
+
+plt.savefig("period_distribution.png")
---
+++ training_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+col_names = ["hist", "norm", "year"]
+df = pd.read_csv("data/breton_devri.csv", sep=",", names=col_names)
+
+df_raw = df[df["year"].between(1900, 2000)]
+df_raw = df_raw.drop(columns=["year"])
+print(len(df_raw))
+
+train, test = train_test_split(df_raw, test_size=0.2)
+print(len(train))
+print(len(test))
+
+train.to_csv("data/period_7_train.csv", sep="\t", header=False, index=False)
+test.to_csv("data/period_7_test.csv", sep="\t", header=False, index=False)
